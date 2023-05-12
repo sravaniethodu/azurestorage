83c1895fdd226a98dc13055169a8b6533e37bef9
sravaniethodu <131959603+sravaniethodu@users.noreply.github.com>
2023-05-12 13:09:59 +0530

    commit

---
+++ Controllers/StorageController.cs
@@ -0,0 +1,83 @@
+﻿using AzureBlobStorage.WebApi.Models;
+using AzureBlobStorage.WebApi.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AzureBlobStorage.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StorageController : ControllerBase
+    {
+        private readonly IAzureStorage _storage;
+
+        public StorageController(IAzureStorage storage)
+        {
+            _storage = storage;
+        }
+        /// <summary>
+        ///  Get all files at the Azure Storage Location and return them
+        /// </summary>
+        /// <returns> Returns an empty array if no files are present at the storage container</returns>
+        [HttpGet(nameof(Get))]
+        public async Task<IActionResult> Get()
+        {
+            List<BlobDto>? files = await _storage.ListAsync();
+            return StatusCode(StatusCodes.Status200OK, files);
+        }
+        /// <summary>
+        /// upload file/files to storage
+        /// </summary>
+        /// <param name="file">files to be uploaded</param>
+        /// <returns>return an error with details to the client/Return a success message to the client about successfull upload</returns>
+        [HttpPost(nameof(Upload))]
+        public async Task<IActionResult> Upload(IFormFile file)
+        {
+            BlobResponseDto? response = await _storage.UploadAsync(file);
+            if (response.Error == true)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, response.Status);
+            }
+            else
+            {
+                return StatusCode(StatusCodes.Status200OK, response);
+            }
+        }
+        /// <summary>
+        /// download file from storage
+        /// </summary>
+        /// <param name="filename">filename which need to be downloaded</param>
+        /// <returns>return error message to client/File was found, return it to client</returns>
+        [HttpGet("{filename}")]
+        public async Task<IActionResult> Download(string filename)
+        {
+            BlobDto? file = await _storage.DownloadAsync(filename);
+
+            if (file == null)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, $"File {filename} could not be downloaded.");
+            }
+            else
+            {
+               // return Ok(file.Name);
+                return File(file.Content, file.ContentType, file.Name);
+            }
+        }
+        /// <summary>
+        /// delete file from storage
+        /// </summary>
+        /// <param name="filename">filename to be deleted</param>
+        /// <returns>error message to the client/ File has been successfully deleted message</returns>
+        [HttpDelete("filename")]
+        public async Task<IActionResult> Delete(string filename)
+        {
+            BlobResponseDto response = await _storage.DeleteAsync(filename);
+            if (response.Error == true)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, response.Status);
+            } else
+            {
+                return StatusCode(StatusCodes.Status200OK, response.Status);
+            }
+        }
+    }
+}
---
+++ Repository/AzureStorage.cs
@@ -0,0 +1,148 @@
+﻿using Azure;
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using AzureBlobStorage.WebApi.Models;
+using AzureBlobStorage.WebApi.Services;
+
+namespace AzureBlobStorage.WebApi.Repository
+{
+    public class AzureStorage : IAzureStorage
+    {
+        #region Dependency Injection / Constructor
+
+        private readonly string _storageConnectionString;
+        private readonly string _storageContainerName;
+        private readonly ILogger<AzureStorage> _logger;
+
+        public AzureStorage(IConfiguration configuration, ILogger<AzureStorage> logger)
+        {
+            _storageConnectionString = configuration.GetValue<string>("BlobConnectionString");
+            _storageContainerName = configuration.GetValue<string>("BlobContainerName");
+            _logger = logger;
+        }
+
+        #endregion
+
+        public async Task<BlobResponseDto> DeleteAsync(string blobFilename)
+        {
+            BlobContainerClient client = new BlobContainerClient(_storageConnectionString, _storageContainerName);
+
+            BlobClient file = client.GetBlobClient(blobFilename);
+
+            try
+            {
+                await file.DeleteAsync();
+            }
+            catch (RequestFailedException ex)
+                when (ex.ErrorCode == BlobErrorCode.BlobNotFound)
+            {
+                _logger.LogError($"File {blobFilename} was not found.");
+                return new BlobResponseDto { Error = true, Status = $"File with name {blobFilename} not found." };
+            }
+
+            return new BlobResponseDto { Error = false, Status = $"File: {blobFilename} has been successfully deleted." };
+
+        }
+
+        public async Task<BlobDto> DownloadAsync(string blobFilename)
+        {
+         
+            BlobContainerClient client = new BlobContainerClient(_storageConnectionString, _storageContainerName);
+
+            try
+            {
+                BlobClient file = client.GetBlobClient(blobFilename);
+
+              
+                if (await file.ExistsAsync())
+                {
+                    var data = await file.OpenReadAsync();
+                    Stream blobContent = data;
+
+                    var content = await file.DownloadContentAsync();
+
+                    string name = blobFilename;
+                    string contentType = content.Value.Details.ContentType;
+
+                    return new BlobDto { Content = blobContent, Name = name, ContentType = contentType };
+                }
+            }
+            catch (RequestFailedException ex)
+                when(ex.ErrorCode == BlobErrorCode.BlobNotFound)
+            {
+                _logger.LogError($"File {blobFilename} was not found.");
+            }
+
+            return null;
+        }
+
+        public async Task<List<BlobDto>> ListAsync()
+        {
+            BlobContainerClient container = new BlobContainerClient(_storageConnectionString, _storageContainerName);
+
+            List<BlobDto> files = new List<BlobDto>();
+
+            await foreach (BlobItem file in container.GetBlobsAsync())
+            {
+                 string uri = container.Uri.ToString();
+                var name = file.Name;
+                var fullUri = $"{uri}/{name}";
+
+                files.Add(new BlobDto {
+                    Uri = fullUri,
+                    Name = name,
+                    ContentType = file.Properties.ContentType
+                });
+            }
+
+            return files;
+        }
+
+        public async Task<BlobResponseDto> UploadAsync(IFormFile blob)
+        {
+            BlobResponseDto response = new();
+
+            BlobContainerClient container = new BlobContainerClient(_storageConnectionString, _storageContainerName);
+
+            await container.CreateIfNotExistsAsync();
+
+            try
+            {
+                    BlobClient client = container.GetBlobClient(blob.FileName);
+
+                await using (Stream? data = blob.OpenReadStream())
+                {
+                 
+                    await client.UploadAsync(data);
+                }
+
+                response.Status = $"File {blob.FileName} Uploaded Successfully";
+                response.Error = false;
+                response.Blob.Uri = client.Uri.AbsoluteUri;
+                response.Blob.Name = client.Name;
+
+            }
+            
+            catch (RequestFailedException ex)
+                when (ex.ErrorCode == BlobErrorCode.BlobAlreadyExists)
+            {
+                _logger.LogError($"File with name {blob.FileName} already exists in container. Set another name to store the file in the container: '{_storageContainerName}.'");
+                response.Status = $"File with name {blob.FileName} already exists. Please use another name to store your file.";
+                response.Error = true;
+                return response;
+            } 
+      
+            catch (RequestFailedException ex)
+            {
+                
+                _logger.LogError($"Unhandled Exception. ID: {ex.StackTrace} - Message: {ex.Message}");
+                response.Status = $"Unexpected error: {ex.StackTrace}. Check log with StackTrace ID.";
+                response.Error = true;
+                return response;
+            }
+
+        
+            return response;
+        }
+    }
+}
